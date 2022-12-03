e162a08791169b58f378336d6ea718bde7145dc2
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2022-12-03 17:07:10 +0100

    renaming result

--- utils.py
+++ utils.py
@@ -14,7 +14,8 @@ def performance(agents, game: Game, n_games):
     """
     n_wins = 0
     for j in range(n_games):
-        n_wins += game.play(agents)
+        game.play(agents)
+        n_wins += game.result()[0]
     win_rate = (n_wins+1)/(n_games+2)
     std = (win_rate * (1-win_rate)/(n_games+2))**.5
     return win_rate, std
